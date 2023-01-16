a4367d3a65ba449d3ef02a16cda803d12dbd04e4
amlWalaed <109831373+amlWalaed@users.noreply.github.com>
2023-01-16 22:03:07 +0200

    add active class and smooth scroll behavior to landing page

--- js/app.js
+++ js/app.js
@@ -37,13 +37,56 @@
  * Begin Main Functions
  * 
 */
-
 // build the nav
 
+let ul = document.querySelector('#navbar__list')
+let sections = document.querySelectorAll('section');
+let fragment = document.createDocumentFragment()
+sections.forEach(function(section) {
+    let li = document.createElement('li')
+    li.dataset.nav =section.dataset.nav 
+    li.classList.add('menu__link')  
+    let menuLink = document.createElement('a')
+    menuLink.innerHTML = section.dataset.nav
+    menuLink.addEventListener('click',function(event){
+    event.preventDefault();
+    section.scrollIntoView({
+    behavior:"smooth"
+    })
+    })
+    li.appendChild(menuLink)
+    fragment.appendChild(li)
+})
+ul.append(fragment)
 
 // Add class 'active' to section when near top of viewport
-
-
+let observer = new IntersectionObserver(function(entries){
+    let section = entries[0]
+    console.log(section.isIntersecting)
+    section.target.classList.remove('your-active-class')
+    if(section.isIntersecting){
+        section.target.classList.add('your-active-class')
+        const menuLinks = document.querySelectorAll('.menu__link')
+        menuLinks.forEach(link=>{
+            link.classList.remove('active')
+            if(link.textContent === section.target.dataset.nav){
+                console.log(link)
+                link.classList.add('active')
+            }
+        })
+    }else{
+        section.target.classList.remove('your-active-class')
+    }
+} , {
+    root:null,
+    rootMargin:"0px",
+    threshold:0.5
+})
+window.addEventListener('scroll',()=>{
+    sections.forEach( section => {
+        observer.observe(section)
+    })
+})
 // Scroll to anchor ID using scrollTO event
 
 
